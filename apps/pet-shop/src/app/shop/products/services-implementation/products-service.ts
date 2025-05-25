351ab94ac2ae9432fce5db71b12ae5138c42b159
derfex <derfex@yandex.ru>
2025-05-25 13:49:08 +0300

    Refactor (apps\pet-shop): Move `products-service.ts`

--- apps/pet-shop/src/app/shop/products/services-implementation/products-service.ts
+++ apps/pet-shop/src/app/shop/products/services-implementation/products-service.ts
@@ -1,8 +1,11 @@
 import { Observable } from 'rxjs';
-import { IProductTableViewModel } from 'src/app/shop/products/shared/product-table-view.model';
+import { IProductTableViewModel } from '../shared/product-table-view.model';
+
+// TODO: Rename the file to `products-service.types.ts`.
 
 export type ObservableProducts = Observable<IProductTableViewModel[]>;
 
+// TODO: Avoid `I` prefix.
 export interface IProductsService {
   getAll(): ObservableProducts;
 }
