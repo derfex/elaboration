a6a89fca15e894bc8fc6b43c4defa29473663f98
derfex <derfex@yandex.ru>
2025-06-06 22:12:41 +0300

    Refactor (apps/pet-shop): Actualize the titles

--- apps/pet-shop/src/app/app.routes.ts
+++ apps/pet-shop/src/app/app.routes.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [
         (c) => c.HomePageComponent
       ),
     path: '',
+    title: 'Elaboration: Angular. Pet-shop',
   },
   {
     loadComponent: () =>
@@ -14,5 +15,6 @@ export const routes: Routes = [
         './integrator/pages/nx-welcome-page/nx-welcome-page.component'
       ).then((c) => c.NxWelcomePageComponent),
     path: 'nx-welcome',
+    title: 'Elaboration: Angular. NX welcome',
   },
 ];
