b8dd1cf1045f6bcd68bb6fbccea4606a4394bf68
derfex <derfex@yandex.ru>
2025-06-06 22:34:39 +0300

    Refactor (apps/pet-shop): Apply the code style

--- apps/pet-shop/src/app/shop/cart/shared/cart.service.ts
+++ apps/pet-shop/src/app/shop/cart/shared/cart.service.ts
@@ -1,61 +1,57 @@
 // External modules.
-import { Injectable } from '@angular/core';
-import {
-  BehaviorSubject,
-  Observable,
-} from 'rxjs';
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, type Observable } from 'rxjs'
 
 // Internal modules.
-import {
-  ProductModels,
-} from '../../products/shared/product-table-view.model';
+import { type IProductTableViewModel, type ProductModels } from '../../products/shared/product-table-view.model'
 
 // Definitions.
 interface IItemsState {
-  items: ProductModels;
-  keys: Set<number>;
+  items: ProductModels
+  keys: Set<number>
 }
 
 const defaultState: IItemsState = {
   items: [],
   keys: new Set(),
-};
-
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   // region ## Properties
-  private subject = new BehaviorSubject<IItemsState>(defaultState);
+  private subject = new BehaviorSubject<IItemsState>(defaultState)
 
   public get state(): Observable<IItemsState> {
-    return this.subject.asObservable();
+    return this.subject.asObservable()
   }
 
   // endregion ## Properties
 
   // region ## Methods
   public addProducts(products: ProductModels): void {
-    const value = this.subject.getValue();
-    const items = value.items.concat(products);
-    const keys = value.keys;
-    products.forEach(product => keys.add(product.id));
+    const value = this.subject.getValue()
+    const items = value.items.concat(products)
+    const keys = value.keys
+    products.forEach((product: IProductTableViewModel): void => {
+      keys.add(product.id)
+    })
     this.subject.next({
       items,
       keys,
-    });
+    })
   }
 
   public deleteProductByID(id: number): void {
-    const value = this.subject.getValue();
-    const items = value.items.filter(item => item.id !== id);
-    const keys = value.keys;
-    keys.delete(id);
+    const value = this.subject.getValue()
+    const items = value.items.filter((item) => item.id !== id)
+    const keys = value.keys
+    keys.delete(id)
     this.subject.next({
       items,
       keys,
-    });
+    })
   }
 
   // endregion ## Methods
