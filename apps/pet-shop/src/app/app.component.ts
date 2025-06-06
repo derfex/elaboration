f1888a33c033385c038fbb80ccf1d0348dc0a54e
derfex <derfex@yandex.ru>
2025-06-06 22:50:52 +0300

    Refactor (apps/pet-shop): Use `ChangeDetectionStrategy.OnPush`

--- apps/pet-shop/src/app/app.component.ts
+++ apps/pet-shop/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
 
 @Component({
-  imports: [
-    RouterModule,
-  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterModule],
   selector: 'app-root',
   standalone: true, // TODO: Can we delete `standalone: true` for Angular@19?
   styleUrl: './app.component.scss',
