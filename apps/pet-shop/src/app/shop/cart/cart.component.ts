a84431366e5f56528da2a069f4bbe4884b098e9d
derfex <derfex@yandex.ru>
2025-06-06 22:46:13 +0300

    Refactor (apps/pet-shop): Use `ChangeDetectionStrategy.OnPush`

--- apps/pet-shop/src/app/shop/cart/cart.component.ts
+++ apps/pet-shop/src/app/shop/cart/cart.component.ts
@@ -1,8 +1,8 @@
 // External modules.
-import { NgIf } from '@angular/common';
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { MatIconButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
+import { NgIf } from '@angular/common'
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { MatIconButton } from '@angular/material/button'
+import { MatIcon } from '@angular/material/icon'
 import {
   MatCell,
   MatCellDef,
@@ -15,18 +15,16 @@ import {
   MatRowDef,
   MatTable,
   MatTableDataSource,
-} from '@angular/material/table';
-import { Subscription } from 'rxjs';
+} from '@angular/material/table'
+import { Subscription } from 'rxjs'
 
 // Internal modules.
-import {
-  IProductTableViewModel,
-  ProductModels,
-} from '../products/shared/product-table-view.model';
-import { EmptinessComponent } from '../shared/components/emptiness/emptiness.component';
-import { CartService } from './shared/cart.service';
+import { IProductTableViewModel, ProductModels } from '../products/shared/product-table-view.model'
+import { EmptinessComponent } from '../shared/components/emptiness/emptiness.component'
+import { CartService } from './shared/cart.service'
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     MatCell,
     MatCellDef,
@@ -42,6 +40,7 @@ import { CartService } from './shared/cart.service';
     MatTable,
     NgIf,
 
+    // Provided by the app.
     EmptinessComponent,
   ],
   selector: 'app-cart',
@@ -51,26 +50,19 @@ import { CartService } from './shared/cart.service';
 })
 export class CartComponent implements OnInit, OnDestroy {
   // region ## Properties
-  private itemsPrivate: ProductModels = [];
-  protected dataSource: MatTableDataSource<IProductTableViewModel> =
-    new MatTableDataSource<IProductTableViewModel>([]);
-  protected displayedColumns: string[] = [
-    'delete',
-    'number',
-    'name',
-    'parent',
-    'price',
-  ];
-  private subscriptionToCart: Subscription | undefined;
+  private itemsPrivate: ProductModels = []
+  protected dataSource: MatTableDataSource<IProductTableViewModel> = new MatTableDataSource<IProductTableViewModel>([])
+  protected displayedColumns: string[] = ['delete', 'number', 'name', 'parent', 'price']
+  private subscriptionToCart: Subscription | undefined
 
   @Input()
   set items(items: ProductModels) {
-    this.itemsPrivate = items;
-    this.dataSource = new MatTableDataSource<IProductTableViewModel>(items);
+    this.itemsPrivate = items
+    this.dataSource = new MatTableDataSource<IProductTableViewModel>(items)
   }
 
   get items(): ProductModels {
-    return this.itemsPrivate;
+    return this.itemsPrivate
   }
 
   // endregion ## Properties
@@ -80,24 +72,24 @@ export class CartComponent implements OnInit, OnDestroy {
   // region ## Lifecycle hooks
   public ngOnInit(): void {
     this.subscriptionToCart = this.cartService.state.subscribe((payload) => {
-      this.items = payload.items;
-    });
+      this.items = payload.items
+    })
   }
 
   public ngOnDestroy(): void {
     // Unsubscribe to ensure no memory leaks.
-    (this.subscriptionToCart as Subscription).unsubscribe();
+    ;(this.subscriptionToCart as Subscription).unsubscribe()
   }
 
   // endregion ## Lifecycle hooks
 
   // region ## Methods
   protected hasDisplayedData(): boolean {
-    return !!this.dataSource.filteredData.length;
+    return !!this.dataSource.filteredData.length
   }
 
   protected deleteItem(id: number): void {
-    this.cartService.deleteProductByID(id);
+    this.cartService.deleteProductByID(id)
   }
 
   // endregion ## Methods
