217cb551436157d0667045e93d6aa8fb85ec5321
derfex <derfex@yandex.ru>
2025-06-06 22:31:33 +0300

    Refactor (apps/pet-shop): Apply the code style

--- apps/pet-shop/src/app/shared/services/api.service.spec.ts
+++ apps/pet-shop/src/app/shared/services/api.service.spec.ts
@@ -1,13 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { APIService } from './api.service';
+import { TestBed } from '@angular/core/testing'
+import { APIService } from './api.service'
 
 // TODO: Explore the tests, refactor, {migrate to `vitest`?}.
 
-describe('APIService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('APIService', (): void => {
+  beforeEach(() => TestBed.configureTestingModule({}))
 
-  it('should be created', () => {
-    const service: APIService = TestBed.get(APIService);
-    expect(service).toBeTruthy();
-  });
-});
+  it('should be created', (): void => {
+    const service: APIService = TestBed.get(APIService)
+    expect(service).toBeTruthy()
+  })
+})
