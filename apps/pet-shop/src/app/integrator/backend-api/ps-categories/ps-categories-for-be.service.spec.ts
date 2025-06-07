fc626af49fd37dbfea5f50824e4069715adffdb3
derfex <derfex@yandex.ru>
2025-06-07 15:06:04 +0300

    Refactor, Breaking changes (apps/pet-shop): Move `ps-categories-for-be.service.ts`

--- apps/pet-shop/src/app/integrator/backend-api/ps-categories/ps-categories-for-be.service.spec.ts
+++ apps/pet-shop/src/app/integrator/backend-api/ps-categories/ps-categories-for-be.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { CategoriesService } from './categories.service';
+import { CategoriesService } from './ps-categories-for-be.service'
 
 describe('CategoriesService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
