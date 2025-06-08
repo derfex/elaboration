87ed63246855149dcb110217a7f0d3465f3a3835
derfex <derfex@yandex.ru>
2025-06-08 14:43:42 +0300

    Refactor, Breaking changes (apps/pet-shop): Rename `ps-products-local.service.ts`

--- apps/pet-shop/src/app/integrator/data/ps-products/products-local.service.spec.ts
+++ apps/pet-shop/src/app/integrator/data/ps-products/products-local.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing'
-import { ProductsLocalService } from './products-local.service'
+import { PSProductsLocalService } from './ps-products-local.service'
 
 // TODO: Explore the tests, refactor, {migrate to `vitest`?}.
 
-describe('ProductsLocalService', (): void => {
+describe('PSProductsLocalService', (): void => {
   beforeEach((): TestBed => TestBed.configureTestingModule({}))
 
   it('should be created', (): void => {
-    const service: ProductsLocalService = TestBed.get(ProductsLocalService)
+    const service: PSProductsLocalService = TestBed.get(PSProductsLocalService)
     expect(service).toBeTruthy()
   })
 })
