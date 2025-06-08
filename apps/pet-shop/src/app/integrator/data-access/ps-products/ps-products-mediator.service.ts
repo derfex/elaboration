4c2352c05d08de16c3690070ef294e543ce2ef63
derfex <derfex@yandex.ru>
2025-06-08 16:06:22 +0300

    Refactor (apps/pet-shop): Use `PSProductsHTTPService` and `PSProductsLocalService`

--- apps/pet-shop/src/app/integrator/data-access/ps-products/ps-products-mediator.service.ts
+++ apps/pet-shop/src/app/integrator/data-access/ps-products/ps-products-mediator.service.ts
@@ -4,18 +4,25 @@ import { map } from 'rxjs/operators'
 import type { PSProduct } from '../../../../architecture/entities/ps-products/ps-products.type'
 import type { PSProductTableItem } from '../../ui/ps-products/ps-products.type'
 import { PSProductsHTTPService } from '../backend-api/ps-products/ps-products-http.service'
+import { PSProductsLocalService } from '../data/ps-products/ps-products-local.service'
 
 @Injectable({
   providedIn: 'root',
 })
 export class PSProductsMediatorService {
+  // TODO: Create a possibility to change `#preferHTTPService`.
+  #preferHTTPService = true
   readonly psProductsHTTPService = inject(PSProductsHTTPService)
+  readonly psProductsLocalService = inject(PSProductsLocalService)
 
   public readList(): Observable<readonly PSProductTableItem[]> {
-    return this.#readListFromHTTPService()
+    if (this.#preferHTTPService) {
+      return this.#readListFromHTTPService()
+    }
+    return this.#readListFromLocalService()
   }
 
-  #convertProductFromHTTPServiceToTableItem(product: PSProduct): PSProductTableItem {
+  #convertProductToTableItem(product: PSProduct): PSProductTableItem {
     return product
   }
 
@@ -24,7 +31,17 @@ export class PSProductsMediatorService {
       .readList()
       .pipe(
         map((products: readonly PSProduct[]): readonly PSProductTableItem[] =>
-          products.map(this.#convertProductFromHTTPServiceToTableItem),
+          products.map(this.#convertProductToTableItem),
+        ),
+      )
+  }
+
+  #readListFromLocalService(): Observable<readonly PSProductTableItem[]> {
+    return this.psProductsLocalService
+      .readList()
+      .pipe(
+        map((products: readonly PSProduct[]): readonly PSProductTableItem[] =>
+          products.map(this.#convertProductToTableItem),
         ),
       )
   }
