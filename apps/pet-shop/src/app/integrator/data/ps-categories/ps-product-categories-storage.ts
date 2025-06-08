67ab246ff1849bb335c58c37b9cea9e4d1738285
derfex <derfex@yandex.ru>
2025-06-08 13:53:43 +0300

    Data (apps/pet-shop): Improve `ps-product-categories-storage.ts`

--- apps/pet-shop/src/app/integrator/data/ps-categories/ps-product-categories-storage.ts
+++ apps/pet-shop/src/app/integrator/data/ps-categories/ps-product-categories-storage.ts
@@ -2,21 +2,28 @@ import type { PSProductCategory } from '../../../../architecture/entities/ps-pro
 
 export class PSProductCategoriesStorage {
   #counter = 0
-  readonly #itemsMap = new Map<PSProductCategory['id'], PSProductCategory>()
+  readonly #itemsByIDMap = new Map<PSProductCategory['id'], PSProductCategory>()
+  readonly #itemsByNameMap = new Map<PSProductCategory['name'], PSProductCategory>()
 
   public create(data: Omit<PSProductCategory, 'id'>): void {
     const id = ++this.#counter
-    this.#itemsMap.set(id, {
+    const category = {
       id,
       ...data,
-    })
+    }
+    this.#itemsByIDMap.set(id, category)
+    this.#itemsByNameMap.set(data.name, category)
   }
 
-  public read(categoryID: PSProductCategory['id']): PSProductCategory | null {
-    return this.#itemsMap.get(categoryID) ?? null
+  public readByID(categoryID: PSProductCategory['id']): PSProductCategory | null {
+    return this.#itemsByIDMap.get(categoryID) ?? null
+  }
+
+  public readByName(categoryID: PSProductCategory['id']): PSProductCategory | null {
+    return this.#itemsByIDMap.get(categoryID) ?? null
   }
 
   public readList(): readonly PSProductCategory[] {
-    return [...this.#itemsMap].map(([, category]: [number, PSProductCategory]): PSProductCategory => category)
+    return [...this.#itemsByIDMap].map(([, category]: [number, PSProductCategory]): PSProductCategory => category)
   }
 }
