492495317e12ba681c1febe799db94c9cbd3b7c4
derfex <derfex@yandex.ru>
2025-06-08 14:14:23 +0300

    Data (apps/pet-shop): Generate the products

--- apps/pet-shop/src/app/integrator/data/pet-shop.data.ts
+++ apps/pet-shop/src/app/integrator/data/pet-shop.data.ts
@@ -1,9 +1,34 @@
 import { PSProductCategoriesStorage } from './ps-categories/ps-product-categories-storage'
+import { PSProductsStorage } from './ps-products/ps-products-storage'
 
 const psProductCategoriesStorage = new PSProductCategoriesStorage()
 {
   const categoriesNames = ['Food', 'Aquariums', 'Accessories', 'Decorations']
   categoriesNames.forEach((name: string): void => {
     psProductCategoriesStorage.create({ name })
+  })
+}
+
+const psProductsStorage = new PSProductsStorage()
+{
+  const productsBlanks: readonly [string, string, number][] = [
+    ['Accessory 1', 'Accessories', 37000],
+    ['Accessory 2', 'Accessories', 69000],
+    ['Aquarium 1', 'Aquariums', 9000],
+    ['Aquarium 2', 'Aquariums', 6000],
+    ['Food 1', 'Food', 1500],
+    ['Food 2', 'Food', 3000],
+  ]
+  productsBlanks.forEach(([name, parentName, price]: [string, string, number]): void => {
+    const parent = psProductCategoriesStorage.readByName(parentName)
+    if (!parent) {
+      // TODO: Throw an error. Or use a general category.
+      return
+    }
+    psProductsStorage.create({
+      name,
+      parent,
+      price,
+    })
   })
 }
