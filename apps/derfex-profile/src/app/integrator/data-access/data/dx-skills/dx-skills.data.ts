e29b0109d65fe6ec32a1d4a80771d1592b574cbe
derfex <derfex@yandex.ru>
2025-09-11 21:02:25 +0300

    #76. feat(derfex-profile): extend `DXSkill`

--- apps/derfex-profile/src/app/integrator/data-access/data/dx-skills/dx-skills.data.ts
+++ apps/derfex-profile/src/app/integrator/data-access/data/dx-skills/dx-skills.data.ts
@@ -4,25 +4,31 @@ export const dxSkills = [
   {
     codename: 'HTML5' as DXSkillCodename,
     name: 'HTML',
+    url: 'https://developer.mozilla.org/docs/Web/HTML',
   },
   {
     codename: 'CSS' as DXSkillCodename,
     name: 'CSS',
+    url: 'https://developer.mozilla.org/docs/Web/CSS',
   },
   {
     codename: 'JS' as DXSkillCodename,
     name: 'JavaScript',
+    url: 'https://developer.mozilla.org/docs/Web/JavaScript',
   },
   {
     codename: 'TS' as DXSkillCodename,
     name: 'TypeScript',
+    url: 'https://www.typescriptlang.org',
   },
   {
     codename: 'NodeJS' as DXSkillCodename,
     name: 'Node.js',
+    url: 'https://nodejs.org',
   },
   {
     codename: 'Angular' as DXSkillCodename,
     name: 'Angular',
+    url: 'https://angular.dev',
   },
 ] as const satisfies readonly DXSkill[]
--- apps/derfex-profile/src/architecture/entities/dx-skills/dx-skills.type.ts
+++ apps/derfex-profile/src/architecture/entities/dx-skills/dx-skills.type.ts
@@ -3,6 +3,7 @@ import type { Brand } from '../../brand-type-utility'
 export interface DXSkill {
   readonly codename: DXSkillCodename
   readonly name: string
+  readonly url: string
 }
 
 export type DXSkillCodename = Brand<string, 'dx skill codename'>
