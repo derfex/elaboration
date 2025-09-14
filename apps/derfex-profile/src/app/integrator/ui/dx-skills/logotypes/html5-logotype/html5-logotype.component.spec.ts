e62c3d7c904c0ff93a3ef8b4d92bde02dc65c5be
derfex <derfex@yandex.ru>
2025-09-15 00:41:02 +0300

    #76. feat(derfex-profile → html5-logotype): create the component

---
+++ apps/derfex-profile/src/app/integrator/ui/dx-skills/logotypes/html5-logotype/html5-logotype.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { HTML5LogotypeComponent } from './html5-logotype.component'
+
+describe('HTML5LogotypeComponent', (): void => {
+  let component: HTML5LogotypeComponent
+  let fixture: ComponentFixture<HTML5LogotypeComponent>
+
+  beforeEach(async (): Promise<void> => {
+    await TestBed.configureTestingModule({}).compileComponents()
+
+    fixture = TestBed.createComponent(HTML5LogotypeComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', (): void => {
+    expect(component).toBeTruthy()
+  })
+})
