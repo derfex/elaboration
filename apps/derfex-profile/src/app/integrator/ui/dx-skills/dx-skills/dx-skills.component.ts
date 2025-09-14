5868d8c21019abf14536030c9b4dfbbf54f3214c
derfex <derfex@yandex.ru>
2025-09-14 18:23:53 +0300

    #76. feat(derfex-profile → dx-skills): improve the component

--- apps/derfex-profile/src/app/integrator/ui/dx-skills/dx-skills/dx-skills.component.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-skills/dx-skills/dx-skills.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, input } from '@angular/core'
 import type { DXSkill } from '~entities/dx-skills/dx-skills.type'
+import { DXSkillCardComponent } from '~ui/dx-skills/dx-skill-card/dx-skill-card.component'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [DXSkillCardComponent],
   selector: 'app-dx-skills',
   styleUrl: './dx-skills.component.sass',
   templateUrl: './dx-skills.component.html',
@@ -10,10 +12,4 @@ import type { DXSkill } from '~entities/dx-skills/dx-skills.type'
 export class DXSkillsComponent {
   public readonly skills = input.required<readonly DXSkill[]>()
   public readonly titleText = input.required<string>()
-
-  protected readonly skillsText = computed<string>(() => {
-    return this.skills()
-      .map(({ name }: DXSkill): string => name)
-      .join(', ')
-  })
 }
