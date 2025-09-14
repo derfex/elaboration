a32dd60032e7585b89de529bcc5cd1f8f6418923
derfex <derfex@yandex.ru>
2025-09-15 01:03:26 +0300

    #76. feat(derfex-profile → css-logotype): create the component

---
+++ apps/derfex-profile/src/app/integrator/ui/dx-skills/logotypes/css-logotype/css-logotype.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { CSSLogotypeComponent } from './css-logotype.component'
+
+describe('CSSLogotypeComponent', (): void => {
+  let component: CSSLogotypeComponent
+  let fixture: ComponentFixture<CSSLogotypeComponent>
+
+  beforeEach(async (): Promise<void> => {
+    await TestBed.configureTestingModule({}).compileComponents()
+
+    fixture = TestBed.createComponent(CSSLogotypeComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', (): void => {
+    expect(component).toBeTruthy()
+  })
+})
--- apps/derfex-profile/src/app/integrator/ui/dx-skills/logotypes/dx-skill-logotype-components.util.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-skills/logotypes/dx-skill-logotype-components.util.ts
@@ -1,9 +1,13 @@
 import { newDevError } from '~app/dev/dev-error.util'
 import type { DXSkillCodename } from '~entities/dx-skills/dx-skills.type'
 import { AngularLogotypeComponent } from '~ui/dx-skills/logotypes/angular-logotype/angular-logotype.component'
+import { CSSLogotypeComponent } from '~ui/dx-skills/logotypes/css-logotype/css-logotype.component'
 import { HTML5LogotypeComponent } from '~ui/dx-skills/logotypes/html5-logotype/html5-logotype.component'
 
-export type DXSkillLogotypeComponentType = typeof AngularLogotypeComponent | typeof HTML5LogotypeComponent
+export type DXSkillLogotypeComponentType =
+  | typeof AngularLogotypeComponent
+  | typeof CSSLogotypeComponent
+  | typeof HTML5LogotypeComponent
 
 export class DXSkillLogotypeComponentsUtil {
   static readonly #logotypeComponentsMap: ReadonlyMap<DXSkillCodename, DXSkillLogotypeComponentType> = new Map<
@@ -11,7 +15,7 @@ export class DXSkillLogotypeComponentsUtil {
     DXSkillLogotypeComponentType
   >([
     ['Angular' as DXSkillCodename, AngularLogotypeComponent],
-    ['CSS' as DXSkillCodename, HTML5LogotypeComponent], // TODO: Replace.
+    ['CSS' as DXSkillCodename, CSSLogotypeComponent],
     ['GitHub' as DXSkillCodename, HTML5LogotypeComponent], // TODO: Replace.
     ['HTML5' as DXSkillCodename, HTML5LogotypeComponent],
     ['JS' as DXSkillCodename, AngularLogotypeComponent], // TODO: Replace.
