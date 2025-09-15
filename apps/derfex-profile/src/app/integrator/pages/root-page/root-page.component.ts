34510c4f4af85e57aa985a727c066c1b58481175
derfex <derfex@yandex.ru>
2025-09-15 21:25:14 +0300

    #76. feat(derfex-profile): display `<app-dx-projects-section>`

--- apps/derfex-profile/src/app/integrator/pages/root-page/root-page.component.ts
+++ apps/derfex-profile/src/app/integrator/pages/root-page/root-page.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { LayoutSectionSeparatorComponent } from '~ui-kit/layout/layout-section-separator/layout-section-separator.component'
+import { DXProjectsSectionComponent } from '~ui/dx-projects/dx-projects-section/dx-projects-section.component'
 import { DXSkillsSectionComponent } from '~ui/dx-skills/dx-skills-section/dx-skills-section.component'
 import { HeaderSectionComponent } from '~ui/header-section/header-section.component'
 import { HeroSectionComponent } from '~ui/hero-section/hero-section.component'
@@ -8,6 +9,7 @@ import { HeroSectionComponent } from '~ui/hero-section/hero-section.component'
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     // Provided by the app.
+    DXProjectsSectionComponent,
     DXSkillsSectionComponent,
     HeaderSectionComponent,
     HeroSectionComponent,
