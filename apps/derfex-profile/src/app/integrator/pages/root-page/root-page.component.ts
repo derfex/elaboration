ccb84ff97c1fb121a1dd958672abf9f5d625652a
derfex <derfex@yandex.ru>
2025-09-15 03:09:59 +0300

    #76. feat(derfex-profile → root-page): use `app-hero-section`, `app-layout-section-separator`, `app-dx-skills-section`

--- apps/derfex-profile/src/app/integrator/pages/root-page/root-page.component.ts
+++ apps/derfex-profile/src/app/integrator/pages/root-page/root-page.component.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { LayoutSectionSeparatorComponent } from '~ui-kit/layout/layout-section-separator/layout-section-separator.component'
+import { DXSkillsSectionComponent } from '~ui/dx-skills/dx-skills-section/dx-skills-section.component'
 import { HeaderSectionComponent } from '~ui/header-section/header-section.component'
+import { HeroSectionComponent } from '~ui/hero-section/hero-section.component'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     // Provided by the app.
+    DXSkillsSectionComponent,
     HeaderSectionComponent,
+    HeroSectionComponent,
+    LayoutSectionSeparatorComponent,
   ],
   selector: 'app-root-page',
   styleUrl: './root-page.component.sass',
