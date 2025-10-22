a1fa67169b7f36f9a4645fbdfdacdaa242cda05d
derfex <derfex@yandex.ru>
2025-10-22 19:17:30 +0300

    #76. feat(derfex-profile → dx-activity-card): create the component

---
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { DXActivityCardComponent } from './dx-activity-card.component'
+
+describe('DXActivityCardComponent', (): void => {
+  let component: DXActivityCardComponent
+  let fixture: ComponentFixture<DXActivityCardComponent>
+
+  beforeEach(async (): Promise<void> => {
+    await TestBed.configureTestingModule({}).compileComponents()
+
+    fixture = TestBed.createComponent(DXActivityCardComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should create', (): void => {
+    // TODO?: fixture.detectChanges()
+    expect(component).toBeTruthy()
+  })
+})
---
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-dx-activity-card',
+  styleUrl: './dx-activity-card.component.sass',
+  templateUrl: './dx-activity-card.component.html',
+})
+export class DXActivityCardComponent {
+  public readonly dateEnd = input.required<Date | null>()
+  public readonly dateStart = input.required<Date>()
+  public readonly results = input.required<readonly string[]>()
+  public readonly role = input.required<string>()
+  public readonly shortDescription = input.required<string>()
+  public readonly skills = input.required<readonly string[]>()
+  public readonly skillsTitleText = input.required<string>()
+
+  protected readonly period = computed<string>(() => {
+    return getPeriodTextWithENLocalization(this.dateStart(), this.dateEnd())
+    // TODO?: Calculate length of the period. Example: `2021-01 – настоящее время · 4 г. 3 мес.`
+  })
+}
+
+function getDateText(date: Date): string {
+  const month = (date.getMonth() + 1 + '').padStart(2, '0')
+  return `${date.getFullYear()}-${month}`
+}
+
+function getPeriodTextWithENLocalization(start: Date, end: Date | null): string {
+  const endText = end ? getDateText(end) : 'present'
+  return `${getDateText(start)} – ${endText}`
+}
+
+function getPeriodTextWithRULocalization(start: Date, end: Date | null): string {
+  const endText = end ? getDateText(end) : 'настоящее время'
+  return `${start} — ${endText}`
+}
