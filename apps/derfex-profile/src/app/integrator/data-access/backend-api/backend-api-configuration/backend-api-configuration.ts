fb78286cd25dab49871e6cdbee14e1f236bd18c8
derfex <derfex@yandex.ru>
2025-10-24 23:31:23 +0300

    #76. refactor(derfex-profile → backend-api-configuration): delete the useless function

--- apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.ts
+++ apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.ts
@@ -2,8 +2,4 @@ export function prepareProfileDataBEAPIURL(postfix: string): string {
   return profileDataServerURLPrefix + postfix
 }
 
-export function prepareProfileDataCDNURL(postfix: string): string {
-  return profileDataServerURLPrefix + postfix
-}
-
 const profileDataServerURLPrefix = 'https://derfex.github.io/profile/archives/2025-10-23/'
