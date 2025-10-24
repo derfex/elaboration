9b777977fa91f52065e8fa9f4e64766792829fb9
derfex <derfex@yandex.ru>
2025-10-24 23:59:45 +0300

    #76. feat(derfex-profile → be): correct the link

--- apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.ts
+++ apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.ts
@@ -2,4 +2,4 @@ export function prepareProfileDataBEAPIURL(postfix: string): string {
   return profileDataServerURLPrefix + postfix
 }
 
-const profileDataServerURLPrefix = 'https://derfex.github.io/profile/archives/2025-10-23/'
+const profileDataServerURLPrefix = 'https://derfex.github.io/profile/archives/2025-10-24/'
