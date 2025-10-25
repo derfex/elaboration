fb2f8ca01f9e0aa6b72eb53e9f3b3452c830553b
derfex <derfex@yandex.ru>
2025-10-25 14:17:34 +0300

    #76. refactor(derfex-profile → locale): move `LocaleUtil` usage to the parent

--- apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activities/dx-activities.component.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activities/dx-activities.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
 import type { DXActivityCodename } from '~entities/dx-activity/dx-activity.type'
+import { LocaleUtil } from '~integrator/data-access/locale/locale.util'
 import type { DXActivitiesListItem } from '~ui/dx-activities/dx-activities/dx-activities.type'
 import { DXActivityCardComponent } from '~ui/dx-activities/dx-activity-card/dx-activity-card.component'
 
@@ -31,10 +32,10 @@ export class DXActivitiesComponent {
     shortDescription,
     skills,
   }: DXActivitiesListItem): DXActivityForTemplate {
+    const period = LocaleUtil.getPeriodTextWithRULocalization(periodFrom, periodTo)
     return {
       codename,
-      periodFrom,
-      periodTo,
+      period,
       results,
       role,
       shortDescription,
@@ -45,8 +46,7 @@ export class DXActivitiesComponent {
 
 interface DXActivityForTemplate {
   readonly codename: DXActivityCodename
-  readonly periodFrom: DXActivitiesListItem['periodFrom']
-  readonly periodTo: DXActivitiesListItem['periodTo']
+  readonly period: string
   readonly results: DXActivitiesListItem['results']
   readonly role: DXActivitiesListItem['role']
   readonly shortDescription: DXActivitiesListItem['shortDescription']
--- apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core'
-import { LocaleUtil } from '~integrator/data-access/locale/locale.util'
+import { ChangeDetectionStrategy, Component, input } from '@angular/core'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -8,16 +7,13 @@ import { LocaleUtil } from '~integrator/data-access/locale/locale.util'
   templateUrl: './dx-activity-card.component.html',
 })
 export class DXActivityCardComponent {
-  public readonly periodFrom = input.required<Date>()
-  public readonly periodTo = input.required<Date | null>()
+  // TODO?: Display a caption, like a tag, like a category of an activity.
+  //  Examples: `web-studio`, `independent development.
+  public readonly period = input.required<string>()
+  // TODO?: Display length of the period. Example: `2021-01 – настоящее время · 4 г. 3 мес.`.
   public readonly results = input.required<readonly string[]>()
   public readonly role = input.required<string>()
   public readonly shortDescription = input.required<string>()
   public readonly skills = input.required<readonly string[]>()
   public readonly skillsTitleText = input.required<string>()
-
-  protected readonly period = computed<string>(() => {
-    return LocaleUtil.getPeriodTextWithRULocalization(this.periodFrom(), this.periodTo())
-    // TODO?: Calculate length of the period. Example: `2021-01 – настоящее время · 4 г. 3 мес.`
-  })
 }
