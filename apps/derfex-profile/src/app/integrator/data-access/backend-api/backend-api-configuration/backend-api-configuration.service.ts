d3bec0b3015dd3d2e6c9107c3d6573c4cb842124
derfex <derfex@yandex.ru>
2025-10-25 22:39:37 +0300

    #76. feat(derfex-profile → data-access): create and use `LocaleSwitcherService`

--- apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.service.ts
+++ apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
-import { map, type Observable, shareReplay } from 'rxjs'
+import { map, type Observable, shareReplay, switchMap } from 'rxjs'
 import { assertDefined } from '~app/dev/dev-error.util'
 import { prepareProfileDataBEAPIURL } from '~be/backend-api-configuration/backend-api-configuration'
 import type {
@@ -8,11 +8,20 @@ import type {
   BackendAPIRelativeURLCodenameForBE,
   BackendAPIURLCodename,
 } from '~be/backend-api-configuration/backend-api-configuration.type'
+import { LocaleSwitcherService } from '~integrator/data-access/locale/locale-switcher.service'
+import type { AppLocale } from '~integrator/data-access/locale/locale.type'
 
 @Injectable({
   providedIn: 'root',
 })
 export class BackendAPIConfigurationService {
+  readonly #localeSwitcherService = inject(LocaleSwitcherService)
+
+  readonly #configurationURLMap: ReadonlyMap<AppLocale, string> = new Map<AppLocale, string>([
+    ['EN', prepareProfileDataBEAPIURL('data/settings/backend-api-configuration.json')],
+    ['RU', prepareProfileDataBEAPIURL('data/settings/backend-api-configuration--ru.json')],
+  ])
+
   readonly #relativeURLCodenamesMap: ReadonlyMap<BackendAPIRelativeURLCodenameForBE, BackendAPIURLCodename> =
     new Map<BackendAPIRelativeURLCodenameForBE, BackendAPIURLCodename>([
       ['dxActivitiesSectionRelativeURL', 'sections/dxActivities'],
@@ -55,8 +64,19 @@ export class BackendAPIConfigurationService {
   }
 
   #readConfiguration(httpClient: HttpClient): Observable<BackendAPIConfigurationForBE> {
-    const backendAPIConfigurationURL = prepareProfileDataBEAPIURL('data/settings/backend-api-configuration.json')
-    return httpClient.get<BackendAPIConfigurationForBE>(backendAPIConfigurationURL)
+    return this.#localeSwitcherService.locale.pipe(
+      map((locale: AppLocale): string => {
+        const backendAPIConfigurationURL = this.#configurationURLMap.get(locale)
+        assertDefined<string>(
+          backendAPIConfigurationURL,
+          `[BackendAPIConfigurationService] Wrong data. The locale ('${locale}') does not exist.`,
+        )
+        return backendAPIConfigurationURL
+      }),
+      switchMap((backendAPIConfigurationURL: string) => {
+        return httpClient.get<BackendAPIConfigurationForBE>(backendAPIConfigurationURL)
+      }),
+    )
   }
 }
 
---
+++ apps/derfex-profile/src/app/integrator/data-access/locale/locale-switcher.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core'
+import { BehaviorSubject, type Observable } from 'rxjs'
+import type { AppLocale } from '~integrator/data-access/locale/locale.type'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocaleSwitcherService {
+  readonly #locale = new BehaviorSubject<AppLocale>('RU')
+
+  public get locale(): Observable<AppLocale> {
+    return this.#locale.asObservable()
+  }
+
+  public set locale(locale: AppLocale) {
+    this.#locale.next(locale)
+  }
+}
