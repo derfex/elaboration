9c88f1ca294aeaafd2514a551db9d11f30053eb7
derfex <derfex@yandex.ru>
2025-11-04 22:18:37 +0300

    #76. refactor(derfex-profile → ui): move `LocaleSwitcherComponent`

--- apps/derfex-profile/src/app/integrator/ui-kit/layout/layout-header/layout-header.component.ts
+++ apps/derfex-profile/src/app/integrator/ui-kit/layout/layout-header/layout-header.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { DerfexLogotypeComponent } from '~ui-kit/identity/derfex-logotype/derfex-logotype.component'
-import { LocaleSwitcherComponent } from '~ui/locale-switcher/locale-switcher.component'
+import { LocaleSwitcherComponent } from '~ui/locale-switcher/locale-switcher/locale-switcher.component'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
