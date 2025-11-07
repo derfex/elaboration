11db19a0bc4ae5b6ebfdf10ce4ef1688e5fd9d95
derfex <derfex@yandex.ru>
2025-11-07 15:32:24 +0300

    #76. feat(derfex-profile → web-api): improve `LOCAL_STORAGE`

--- apps/derfex-profile/src/app/integrator/data-access/web-api/local-storage/local-storage.token.ts
+++ apps/derfex-profile/src/app/integrator/data-access/web-api/local-storage/local-storage.token.ts
@@ -2,7 +2,37 @@ import { inject, InjectionToken, PLATFORM_ID } from '@angular/core'
 
 export const LOCAL_STORAGE = new InjectionToken<Storage>('window local storage object', {
   factory: (): Storage => {
-    return inject(PLATFORM_ID) === 'browser' ? window.localStorage : ({} as Storage)
+    if (inject(PLATFORM_ID) === 'browser') {
+      if (storageAvailable()) return window.localStorage
+    }
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      clear(): void {},
+      getItem(key: string): string | null { return null },
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      removeItem(key: string): void {},
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      setItem(key: string, value: string): void {},
+    } as Storage
   },
   providedIn: 'root',
 })
+
+function storageAvailable(): boolean {
+  let storage
+  try {
+    storage = window.localStorage
+    const x = '__storage_test__'
+    storage.setItem(x, x)
+    storage.removeItem(x)
+    return true
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      e.name === 'QuotaExceededError' &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      !!storage &&
+      storage.length !== 0
+    )
+  }
+}
