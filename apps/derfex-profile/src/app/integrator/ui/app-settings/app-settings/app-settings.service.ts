95cc20e44af6d2f93ae85e18295ce41040b7c84b
derfex <derfex@yandex.ru>
2025-11-10 00:13:42 +0300

    #76. feat(derfex-profile → ui): keep `colorScheme` in `localStorage`

--- apps/derfex-profile/src/app/integrator/ui/app-settings/app-settings/app-settings.service.ts
+++ apps/derfex-profile/src/app/integrator/ui/app-settings/app-settings/app-settings.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable, type Renderer2 } from '@angular/core'
-import { map, type Observable } from 'rxjs'
+import { combineLatest, map, noop, type Observable } from 'rxjs'
 import { LocaleSwitcherService } from '~integrator/data-access/locale/locale-switcher.service'
 import type { AppLocale } from '~integrator/data-access/locale/locale.type'
 import { LocalStorageService } from '~integrator/data-access/web-api/local-storage/local-storage.service'
 import { AppThemeSwitcherService } from '~ui-kit/theming/app-theme-switcher.service'
+import type { ThemeColorSchemeCodename } from '~ui-kit/theming/theming.type'
 
 @Injectable({
   providedIn: 'root',
@@ -15,11 +16,9 @@ export class AppSettingsService {
 
   public initAndObserve(renderer: Renderer2): Observable<void> {
     const locale = this.#initAndObserveLocale()
+    const colorScheme = this.#initAndObserveThemeColorScheme(renderer)
 
-    // TODO?: Unsubscribe?
-    this.#appThemeSwitcherService.observePrefersColorScheme(renderer)
-
-    return locale
+    return combineLatest([locale, colorScheme]).pipe(map(noop))
   }
 
   #initAndObserveLocale(): Observable<void> {
@@ -31,6 +30,22 @@ export class AppSettingsService {
     return this.#localeSwitcherService.locale.pipe(
       map((locale: AppLocale): void => {
         this.#localStorageService.setItem('locale', locale)
+      }),
+    )
+  }
+
+  #initAndObserveThemeColorScheme(renderer: Renderer2): Observable<void> {
+    const colorScheme = this.#localStorageService.getItem<ThemeColorSchemeCodename>('colorScheme')
+    if (colorScheme) {
+      this.#appThemeSwitcherService.switchColorScheme(renderer, colorScheme)
+    }
+
+    // TODO?: Unsubscribe?
+    this.#appThemeSwitcherService.observePrefersColorScheme(renderer)
+
+    return this.#appThemeSwitcherService.colorScheme.pipe(
+      map((colorScheme: ThemeColorSchemeCodename): void => {
+        this.#localStorageService.setItem('colorScheme', colorScheme)
       }),
     )
   }
