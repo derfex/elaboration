24b2d545685dbcd442d71d07becf741a4ef6d6f4
derfex <derfex@yandex.ru>
2025-11-12 22:57:36 +0300

    #76. build(derfex-profile → integrator): improve `profileDataServerURLPrefix`

--- apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.ts
+++ apps/derfex-profile/src/app/integrator/data-access/backend-api/backend-api-configuration/backend-api-configuration.ts
@@ -2,4 +2,4 @@ export function prepareProfileDataBEAPIURL(postfix: string): string {
   return profileDataServerURLPrefix + postfix
 }
 
-const profileDataServerURLPrefix = 'https://derfex.github.io/profile/archives/2025-10-24/'
+const profileDataServerURLPrefix = './'
