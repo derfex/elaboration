2c230591dac119f2db2059622152abf52e205d71
derfex <derfex@yandex.ru>
2025-11-18 19:33:48 +0300

    #83. feat(web-invitation → event-program): use `EventProgramSectionMediatorService`

--- apps/web-invitation/src/app/integrator/ui/event-program/event-program-section/event-program-section.component.ts
+++ apps/web-invitation/src/app/integrator/ui/event-program/event-program-section/event-program-section.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component, type OnInit, signal } from '@angular/core'
-import type { EventProgramStageCodename } from '~entities/event-program-stages/event-program-stages.type'
-import type { EventProgramSectionParameters } from '~ui/event-program/event-program-section/event-program-section.type'
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, type OnInit, signal } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { EventProgramSectionMediatorService } from '~be/event-program/event-program-section-mediator.service'
+import type {
+  EventProgramSectionParameters,
+  EventProgramSectionParametersAndList,
+} from '~ui/event-program/event-program-section/event-program-section.type'
 import { EventProgramComponent } from '~ui/event-program/event-program/event-program.component'
 import type { EventProgramStagesListItem } from '~ui/event-program/event-program/event-program.type'
 
@@ -12,14 +16,23 @@ import type { EventProgramStagesListItem } from '~ui/event-program/event-program
   templateUrl: './event-program-section.component.html',
 })
 export class EventProgramSectionComponent implements OnInit {
+  readonly #destroyRef = inject(DestroyRef)
+  readonly #eventProgramSectionMediatorService = inject(EventProgramSectionMediatorService)
+
   protected readonly sectionParameters = signal<EventProgramSectionParameters>({
     titleText: 'No data',
   })
   protected readonly stages = signal<readonly EventProgramStagesListItem[]>([])
 
   public ngOnInit(): void {
-    this.sectionParameters.set({
-      titleText: 'No data',
-    })
+    this.#eventProgramSectionMediatorService
+      .readSectionParametersAndList()
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe(({ list, sectionParameters }: EventProgramSectionParametersAndList): void => {
+        this.sectionParameters.set({
+          titleText: sectionParameters.titleText,
+        })
+        this.stages.set(list)
+      })
   }
 }
