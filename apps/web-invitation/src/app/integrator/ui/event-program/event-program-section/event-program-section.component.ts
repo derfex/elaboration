9039c791c407345e4b38fa53ca9abb58d7c13418
derfex <derfex@yandex.ru>
2025-11-18 18:36:26 +0300

    #83. feat(web-invitation → event-program): use `EventProgramStageCardComponent`

--- apps/web-invitation/src/app/integrator/ui/event-program/event-program-section/event-program-section.component.ts
+++ apps/web-invitation/src/app/integrator/ui/event-program/event-program-section/event-program-section.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, type OnInit, signal } from '@angular/core'
+import type { EventProgramStageCodename } from '~entities/event-program-stages/event-program-stages.type'
 import type { EventProgramSectionParameters } from '~ui/event-program/event-program-section/event-program-section.type'
 import { EventProgramComponent } from '~ui/event-program/event-program/event-program.component'
+import type { EventProgramStagesListItem } from '~ui/event-program/event-program/event-program.type'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,6 +15,7 @@ export class EventProgramSectionComponent implements OnInit {
   protected readonly sectionParameters = signal<EventProgramSectionParameters>({
     titleText: 'No data',
   })
+  protected readonly stages = signal<readonly EventProgramStagesListItem[]>([])
 
   public ngOnInit(): void {
     this.sectionParameters.set({
--- apps/web-invitation/src/app/integrator/ui/event-program/event-program/event-program.component.ts
+++ apps/web-invitation/src/app/integrator/ui/event-program/event-program/event-program.component.ts
@@ -1,11 +1,15 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core'
+import { EventProgramStageCardComponent } from '~ui/event-program/event-program-stage-card/event-program-stage-card.component'
+import type { EventProgramStagesListItem } from '~ui/event-program/event-program/event-program.type'
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [EventProgramStageCardComponent],
   selector: 'app-event-program',
   styleUrl: './event-program.component.sass',
   templateUrl: './event-program.component.html',
 })
 export class EventProgramComponent {
+  public readonly stages = input.required<readonly EventProgramStagesListItem[]>()
   public readonly titleText = input.required<string>()
 }
