a2458deab579952a8773c533b2a016597768c364
derfex <derfex@yandex.ru>
2025-11-21 20:14:18 +0300

    #83. feat(web-invitation → guest-questionnaire): create `GoogleFormComponent`

---
+++ apps/web-invitation/src/app/integrator/ui/guest-questionnaire/google-form/google-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { GoogleFormComponent } from './google-form.component'
+
+describe('GoogleFormComponent', (): void => {
+  let component: GoogleFormComponent
+  let fixture: ComponentFixture<GoogleFormComponent>
+
+  beforeEach(async (): Promise<void> => {
+    await TestBed.configureTestingModule({}).compileComponents()
+
+    fixture = TestBed.createComponent(GoogleFormComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should create', (): void => {
+    fixture.componentRef.setInput('height', 1)
+    fixture.componentRef.setInput('url', 'NoData')
+    // TODO?: fixture.detectChanges()
+    expect(component).toBeTruthy()
+  })
+})
---
+++ apps/web-invitation/src/app/integrator/ui/guest-questionnaire/google-form/google-form.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core'
+import { DomSanitizer, type SafeResourceUrl } from '@angular/platform-browser'
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-google-form',
+  styleUrl: './google-form.component.sass',
+  templateUrl: './google-form.component.html',
+})
+export class GoogleFormComponent {
+  readonly #domSanitizer = inject(DomSanitizer)
+
+  public readonly height = input.required<number>()
+  public readonly url = input.required<string>()
+
+  protected readonly src = computed<SafeResourceUrl>(() => {
+    return this.#domSanitizer.bypassSecurityTrustResourceUrl(this.url())
+  })
+
+  protected readonly style = computed<string>(() => {
+    return `height: ${this.height()}px`
+  })
+}
