ffb5365386edbd00beff14b3c538dce0b3cf2e01
derfex <derfex@yandex.ru>
2025-11-24 16:32:50 +0300

    #83. feat(web-invitation → event-details): use `BackendAPIConfigurationService`

--- apps/web-invitation/src/app/integrator/data-access/backend-api/event-details/event-details-section-mediator.service.ts
+++ apps/web-invitation/src/app/integrator/data-access/backend-api/event-details/event-details-section-mediator.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core'
-import { map, type Observable, of } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { inject, Injectable } from '@angular/core'
+import { map, type Observable, switchMap } from 'rxjs'
+import { BackendAPIConfigurationService } from '~be/backend-api-configuration/backend-api-configuration.service'
 import type { EventDetailsSectionParametersForBE } from '~be/event-details/event-details-section-for-be.type'
 import type { EventDetailsSectionParameters } from '~ui/event-details/event-details-section/event-details-section.type'
 
@@ -7,6 +9,9 @@ import type { EventDetailsSectionParameters } from '~ui/event-details/event-deta
   providedIn: 'root',
 })
 export class EventDetailsSectionMediatorService {
+  readonly #backendAPIConfigurationService = inject(BackendAPIConfigurationService)
+  readonly #httpClient = inject(HttpClient)
+
   public readSectionParameters(): Observable<EventDetailsSectionParameters> {
     return this.#readSectionParametersAsUncompiled().pipe(
       map<EventDetailsSectionParametersForBE, EventDetailsSectionParameters>(
@@ -19,9 +24,18 @@ export class EventDetailsSectionMediatorService {
   }
 
   #readSectionParametersAsUncompiled(): Observable<EventDetailsSectionParametersForBE> {
-    return of({
-      descriptionParagraphs: [],
-      titleText: 'No data',
-    })
+    return this.#readURLForUncompiled().pipe(
+      switchMap((url: string): Observable<EventDetailsSectionParametersForBE> => {
+        return this.#readSectionParametersAsUncompiledByURL(url)
+      }),
+    )
+  }
+
+  #readSectionParametersAsUncompiledByURL(url: string): Observable<EventDetailsSectionParametersForBE> {
+    return this.#httpClient.get<EventDetailsSectionParametersForBE>(url)
+  }
+
+  #readURLForUncompiled(): Observable<string> {
+    return this.#backendAPIConfigurationService.readURL('sections/eventDetails')
   }
 }
