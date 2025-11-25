a00b43a3b122e620b93ff81b773932b2b9a24107
derfex <derfex@yandex.ru>
2025-11-26 00:04:03 +0300

    #83. test(web-invitation → app-hero): set the inputs

--- apps/web-invitation/src/app/integrator/ui/app-hero-section/app-hero/app-hero.component.spec.ts
+++ apps/web-invitation/src/app/integrator/ui/app-hero-section/app-hero/app-hero.component.spec.ts
@@ -13,6 +13,10 @@ describe('AppHeroComponent', (): void => {
   })
 
   it('should create', (): void => {
+    fixture.componentRef.setInput('illustrationImageAltText', 'No data')
+    fixture.componentRef.setInput('illustrationImageHeight', 42)
+    fixture.componentRef.setInput('illustrationImageURL', 'NoData')
+    fixture.componentRef.setInput('illustrationImageWidth', 42)
     fixture.componentRef.setInput('phraseText', 'No data.')
     // TODO?: fixture.detectChanges()
     expect(component).toBeTruthy()
