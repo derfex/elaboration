f38d73a540dbe43ad4011bdf468f775846e77a60
derfex <derfex@yandex.ru>
2025-11-25 12:52:40 +0300

    #83. refactor(web-invitation → app-hero-section): move the component (amend)

--- apps/web-invitation/src/app/integrator/data-access/backend-api/app/app-sections-mediator.service.ts
+++ apps/web-invitation/src/app/integrator/data-access/backend-api/app/app-sections-mediator.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { map, type Observable, of, shareReplay } from 'rxjs'
 import type { AppSectionsParametersForBE } from '~be/app/app-sections-for-be.type'
 import type { AppFooterSectionParameters } from '~ui/app-footer-section/app-footer-section.type'
-import type { AppHeroSectionParameters } from '~ui/app-hero-section/app-hero-section.type'
+import type { AppHeroSectionParameters } from '~ui/app-hero-section/app-hero-section/app-hero-section.type'
 
 @Injectable({
   providedIn: 'root',
