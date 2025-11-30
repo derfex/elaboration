78ca8272a4797682223bc0a739457ca77030578f
derfex <derfex@yandex.ru>
2025-12-01 00:46:47 +0300

    #83. test(web-invitation → dress-code): actualize the inputs' initialization

--- apps/web-invitation/src/app/integrator/ui/dress-code/dress-code/dress-code.component.spec.ts
+++ apps/web-invitation/src/app/integrator/ui/dress-code/dress-code/dress-code.component.spec.ts
@@ -15,6 +15,10 @@ describe('DressCodeComponent', (): void => {
   it('should create', (): void => {
     fixture.componentRef.setInput('descriptionParagraphs', ['No data', 'No data'])
     fixture.componentRef.setInput('tints', ['#042'])
+    fixture.componentRef.setInput('illustrationImageAltText', 'No data')
+    fixture.componentRef.setInput('illustrationImageHeight', 42)
+    fixture.componentRef.setInput('illustrationImageURL', 'NoData')
+    fixture.componentRef.setInput('illustrationImageWidth', 42)
     fixture.componentRef.setInput('titleText', 'No data')
     // TODO?: fixture.detectChanges()
     expect(component).toBeTruthy()
