fbc81659b931b40c4ebfad1c99952e238daca633
derfex <derfex@yandex.ru>
2025-12-07 23:02:54 +0300

    #83. test(web-invitation → event-organization): actualize the inputs' initialization

--- apps/web-invitation/src/app/integrator/ui/event-organization/event-organization/event-organization.component.spec.ts
+++ apps/web-invitation/src/app/integrator/ui/event-organization/event-organization/event-organization.component.spec.ts
@@ -1,4 +1,5 @@
 import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import type { EventOrganizationContact } from '~ui/event-organization/event-organization/event-organization.type'
 import { EventOrganizationComponent } from './event-organization.component'
 
 describe('EventOrganizationComponent', (): void => {
@@ -10,10 +11,14 @@ describe('EventOrganizationComponent', (): void => {
 
     fixture = TestBed.createComponent(EventOrganizationComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', (): void => {
+    const contacts: readonly EventOrganizationContact[] = [{ hRef: 'NoData', titleText: 'No data' }]
+    fixture.componentRef.setInput('contacts', contacts)
+    fixture.componentRef.setInput('descriptionParagraphs', ['No data'])
+    fixture.componentRef.setInput('titleText', 'No data')
+    // TODO?: fixture.detectChanges()
     expect(component).toBeTruthy()
   })
 })
