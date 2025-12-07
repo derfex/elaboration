87d7dc5a8611de46e05d0f132ed5e9ea342d45f5
derfex <derfex@yandex.ru>
2025-12-07 23:02:39 +0300

    #83. test(web-invitation → event-day): actualize the inputs' initialization

--- apps/web-invitation/src/app/integrator/ui/event-day/event-day/event-day.component.spec.ts
+++ apps/web-invitation/src/app/integrator/ui/event-day/event-day/event-day.component.spec.ts
@@ -10,10 +10,15 @@ describe('EventDayComponent', (): void => {
 
     fixture = TestBed.createComponent(EventDayComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', (): void => {
+    fixture.componentRef.setInput('appealText', 'No data')
+    fixture.componentRef.setInput('authorsSignatureLines', ['No data', 'No data'])
+    fixture.componentRef.setInput('eventDateCaptionText', 'No data')
+    fixture.componentRef.setInput('eventDateValueText', 'No data')
+    fixture.componentRef.setInput('titleText', 'No data')
+    // TODO?: fixture.detectChanges()
     expect(component).toBeTruthy()
   })
 })
