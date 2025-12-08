b24618ca2a9d457b3b1d89ca8cdaa987b7e944f8
derfex <derfex@yandex.ru>
2025-12-08 12:22:18 +0300

    #83. test(web-invitation → event-details-section): create the file

---
+++ apps/web-invitation/src/app/integrator/ui/event-details/event-details-section/event-details-section.component.spec.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core'
+import { type ComponentFixture, TestBed } from '@angular/core/testing'
+import { type Observable, of } from 'rxjs'
+import { EventDetailsSectionMediatorService } from '~be/event-details/event-details-section-mediator.service'
+import type { EventDetailsSectionParameters } from '~ui/event-details/event-details-section/event-details-section.type'
+import { EventDetailsSectionComponent } from './event-details-section.component'
+
+describe('EventDetailsSectionComponent', (): void => {
+  let component: EventDetailsSectionComponent
+  let fixture: ComponentFixture<EventDetailsSectionComponent>
+
+  beforeEach(async (): Promise<void> => {
+    await TestBed.configureTestingModule({
+      imports: [
+        // Stubs.
+        EventDetailsStubComponent,
+      ],
+      providers: [{ provide: EventDetailsSectionMediatorService, useClass: EventDetailsSectionMediatorStubService }],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(EventDetailsSectionComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', (): void => {
+    expect(component).toBeTruthy()
+  })
+})
+
+@Component({ selector: 'app-event-details', template: '' })
+class EventDetailsStubComponent {}
+
+class EventDetailsSectionMediatorStubService {
+  public readSectionParameters(): Observable<EventDetailsSectionParameters> {
+    return of({
+      descriptionParagraphs: ['Test data.'],
+      titleText: 'Test data',
+      wishes: [],
+    })
+  }
+}
