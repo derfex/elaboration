1bce875e1a1f8f2f6d03b3573811479525e2b0dc
derfex <derfex@yandex.ru>
2025-12-08 14:46:23 +0300

    #83. test(web-invitation → event-details-section): improve the inputs' initialization

--- apps/web-invitation/src/app/integrator/ui/event-details/event-details-section/event-details-section.component.spec.ts
+++ apps/web-invitation/src/app/integrator/ui/event-details/event-details-section/event-details-section.component.spec.ts
@@ -3,6 +3,7 @@ import { type ComponentFixture, TestBed } from '@angular/core/testing'
 import { type Observable, of } from 'rxjs'
 import { EventDetailsSectionMediatorService } from '~be/event-details/event-details-section-mediator.service'
 import type { EventDetailsSectionParameters } from '~ui/event-details/event-details-section/event-details-section.type'
+import type { EventDetailsWish } from '~ui/event-details/event-details/event-details.type'
 import { EventDetailsSectionComponent } from './event-details-section.component'
 
 describe('EventDetailsSectionComponent', (): void => {
@@ -33,10 +34,19 @@ class EventDetailsStubComponent {}
 
 class EventDetailsSectionMediatorStubService {
   public readSectionParameters(): Observable<EventDetailsSectionParameters> {
+    const wishes: readonly EventDetailsWish[] = [
+    {
+      text: 'Test data',
+    },
+    {
+      iconImageURL: 'TestData',
+      text: 'Test data',
+    },
+  ]
     return of({
       descriptionParagraphs: ['Test data.'],
       titleText: 'Test data',
-      wishes: [],
+      wishes,
     } satisfies EventDetailsSectionParameters)
   }
 }
