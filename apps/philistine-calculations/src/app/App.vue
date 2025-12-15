10b0459d90f9fccb003fd0aba77c60fa91ecc36c
derfex <derfex@yandex.ru>
2025-12-16 00:50:08 +0300

    #91. feat(philistine-calculations → AccrualsOnBankDeposits): delete the useless navigation

--- apps/philistine-calculations/src/app/App.vue
+++ apps/philistine-calculations/src/app/App.vue
@@ -1,14 +1,8 @@
 <script lang="ts" setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
   <RouterView />
 </template>
 
