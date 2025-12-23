282bf1031786dbe5f431fb28b67d9142450f9028
derfex <derfex@yandex.ru>
2025-12-24 00:44:03 +0300

    #76. feat(derfex-profile-data-server): create and use `SaveDataService`

--- derfex-profile-data-server/src/app/app.controller.ts
+++ derfex-profile-data-server/src/app/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { AppService } from './app.service'
 import type { AppServiceData } from './app.service.type'
+import { SaveDataService } from './save-data/save-data.service'
+import type { SaveFilePostRequestBody, SaveFilePostResponse } from './save-data/save-data.type'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly saveDataService: SaveDataService,
+  ) {}
 
   @Get()
   public getData(): AppServiceData {
     return this.appService.getData()
+  }
+
+  @Post('save-file')
+  public async saveFile(@Body() body: SaveFilePostRequestBody): Promise<SaveFilePostResponse> {
+    return this.saveDataService.saveFile(body)
   }
 }
