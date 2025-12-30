ee1748bd6e12f08ae49fceafdc2de74850438422
derfex <derfex@yandex.ru>
2025-12-30 12:44:44 +0300

    #76. feat(derfex-profile → dx-activities): improve the card, add `achievementsTitleText`

--- apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.spec.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.spec.ts
@@ -13,6 +13,13 @@ describe('DXActivityCardComponent', (): void => {
   })
 
   it('should create', (): void => {
+    // fixture.componentRef.setInput('achievements', ['Test data.'])
+    // fixture.componentRef.setInput('achievementsTitleText', 'Test data')
+    // fixture.componentRef.setInput('period', '2020-02 – 2020-02')
+    // fixture.componentRef.setInput('role', 'Test data')
+    // fixture.componentRef.setInput('shortDescription', 'Test data.')
+    // fixture.componentRef.setInput('skills', ['Test data', 'Test data 42'])
+    // fixture.componentRef.setInput('skillsTitleText', 'Test data')
     // TODO?: fixture.detectChanges()
     expect(component).toBeTruthy()
   })
--- apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
@@ -7,11 +7,12 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core'
   templateUrl: './dx-activity-card.component.html',
 })
 export class DXActivityCardComponent {
+  public readonly achievements = input.required<readonly string[]>()
+  public readonly achievementsTitleText = input.required<string>()
   // TODO?: Display a caption, like a tag, like a category of an activity.
   //  Examples: `web-studio`, `independent development.
   public readonly period = input.required<string>()
   // TODO?: Display length of the period. Example: `2021-01 – настоящее время · 4 г. 3 мес.`.
-  public readonly results = input.required<readonly string[]>()
   public readonly role = input.required<string>()
   public readonly shortDescription = input.required<string>()
   public readonly skills = input.required<readonly string[]>()
