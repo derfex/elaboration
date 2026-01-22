7bcc47b0f01b02b141c46bcb202823ef022a2532
derfex <derfex@yandex.ru>
2026-01-22 22:45:46 +0300

    #91. feat(philistine-calculations → HomeView): add the switching

--- apps/philistine-calculations/src/integrator/views/HomeView.vue
+++ apps/philistine-calculations/src/integrator/views/HomeView.vue
@@ -1,9 +1,47 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import AccrualsOnBankDepositsSection from '|ui/accruals-on-bank-deposits/AccrualsOnBankDepositsSection.vue'
+
+// # Uses in the template
+
+const accrualsOnBankDepositsSectionIsShown = ref(false)
+function accrualsOnBankDepositsSectionTitleContainerClickHandler(): void {
+  accrualsOnBankDepositsSectionIsShown.value = true
+}
+const accrualsOnBankDepositsSectionTitleText = 'Accruals on a bank deposit'
 </script>
 
 <template>
   <main>
-    <AccrualsOnBankDepositsSection />
+    <template v-if="accrualsOnBankDepositsSectionIsShown">
+      <AccrualsOnBankDepositsSection />
+    </template>
+    <template v-else>
+      <div class="app-title-container" @click="accrualsOnBankDepositsSectionTitleContainerClickHandler">
+        <h2 class="app-title">
+          {{ accrualsOnBankDepositsSectionTitleText }}
+        </h2>
+      </div>
+    </template>
   </main>
 </template>
+
+<style lang="sass" scoped>
+@use '../ui-kit/ui-kit'
+
+
+$_app-padding: 20px
+
+
+.app-title-container
+  @include ui-kit.app-ui-kit_glass-mixin
+
+  margin-block: $_app-padding
+  border-radius: 4px
+  padding: $_app-padding
+  cursor: pointer
+
+.app-title
+  @include ui-kit.app-ui-kit_h2-mixin
+  @include ui-kit.app-ui-kit_neon-mixin
+</style>
