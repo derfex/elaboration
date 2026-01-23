82653f652e3e0365b41ee91b38cdf57a8cdcd442
derfex <derfex@yandex.ru>
2026-01-23 15:01:31 +0300

    #91. refactor(philistine-calculations → RoundedNumber): create and use `rounded-number.ts` (amend)

--- apps/philistine-calculations/src/architecture/logic/rounded-number/rounded-number.ts
+++ apps/philistine-calculations/src/architecture/logic/rounded-number/rounded-number.ts
@@ -1,8 +1,3 @@
-export interface RoundedNumberParts {
-  readonly essential: string
-  readonly minor: string
-}
-
 export function calculateRoundedNumberParts(
   number: number,
   afterPointEssentialCount: number,
@@ -10,11 +5,6 @@ export function calculateRoundedNumberParts(
 ): RoundedNumberParts {
   const point = '.'
   const minus = '–'
-  // if (number === 0) {
-  //   const essential = `0${point}${'0'.repeat(afterPointEssentialCount)}`
-  //   const minor = '0'.repeat(afterPointMinorCount)
-  //   return { essential, minor }
-  // }
   const wholeSign = number >= 0 ? '' : minus
   const absValue = Math.abs(number)
   const afterPointCount = afterPointEssentialCount + afterPointMinorCount
@@ -24,10 +14,15 @@ export function calculateRoundedNumberParts(
   const text = roundedAbsValueAsString.padStart(wholeMinCount + afterPointCount, '0')
   const wholeCount = text.length - afterPointCount
   const whole = text.substring(0, wholeCount)
-  // console.log({ number, text, wholeCount })
   const minorStart = text.length - afterPointMinorCount
   const essentialAfterPoint = text.substring(wholeCount, minorStart)
   const minor = text.substring(minorStart)
   const essential = `${wholeSign}${whole}${point}${essentialAfterPoint}`
   return { essential, minor }
+}
+
+
+interface RoundedNumberParts {
+  readonly essential: string
+  readonly minor: string
 }
