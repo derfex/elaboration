331f5d68237c53de0455db6634ab4d8cfea58e3c
derfex <derfex@yandex.ru>
2026-01-30 00:29:37 +0300

    #91. fix(philistine-calculations → HomeView): use `.app-section__wrapper`

--- apps/philistine-calculations/src/integrator/views/HomeView.vue
+++ apps/philistine-calculations/src/integrator/views/HomeView.vue
@@ -17,30 +17,45 @@ const navLinks = [
 
 <template>
   <main>
-    <nav>
-      <ul>
-        <template
-          v-for="item in navLinks"
-          :key="item.routeName"
-        >
-          <li>
-            <RouterLink
-              class="app-nav-link"
-              :to="{ name: item.routeName }"
-              exact-active-class="app-nav-link--exact-active"
-            >
-              {{ item.captionText }}
-            </RouterLink>
-          </li>
-        </template>
-      </ul>
-    </nav>
+    <div class="app-component-independent-root">
+      <div class="app-section__wrapper">
+        <div class="app-section__content">
+          <nav>
+            <ul class="app-nav-list">
+              <template v-for="item in navLinks" :key="item.routeName">
+                <li>
+                  <RouterLink
+                    class="app-nav-link"
+                    :to="{ name: item.routeName }"
+                    exact-active-class="app-nav-link--exact-active"
+                  >
+                    {{ item.captionText }}
+                  </RouterLink>
+                </li>
+              </template>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
     <RouterView />
   </main>
 </template>
 
 <style lang="sass" scoped>
+@use '../ui-kit/layout/layout'
+@use '../ui-kit/reset'
 @use '../ui-kit/ui-kit'
+
+.app-component-independent-root
+  padding-block: 20px
+
+.app-section__content
+  padding-inline: 20px
+
+.app-section__wrapper
+  @include layout.app-layout_section-wrapper-mixin
+
 
 .app-nav-link
   color: var(--app-color--accent--brand)
@@ -50,6 +65,9 @@ const navLinks = [
 
 .app-nav-link--exact-active::before
   content: '→ '
+
+.app-nav-list
+  @include reset.app-reset_ul-mixin
 
 .app-title
   @include ui-kit.app-ui-kit_h2-mixin
