53bde79c9064032764e7e81bb98d5362d5c0888b
derfex <derfex@yandex.ru>
2026-02-12 19:46:21 +0300

    #107. feat(vehicles-registry-front-end → integrator): use `vuetify`

--- apps/vehicles-registry-front-end/src/main.ts
+++ apps/vehicles-registry-front-end/src/main.ts
@@ -3,5 +3,10 @@ import './styles.sass'
 import { createApp } from 'vue'
 import App from './app/App.vue'
 
+import vuetify from './integrator/plugins/vuetify'
+
 const app = createApp(App)
+
+app.use(vuetify)
+
 app.mount('#root')
