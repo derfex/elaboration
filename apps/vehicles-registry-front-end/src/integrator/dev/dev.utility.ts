e8742911899a639be2d537cc8b9bd1db3e5be26c
derfex <derfex@yandex.ru>
2026-02-14 00:56:28 +0300

    #107. feat(vehicles-registry-front-end → integrator): reorganize `dev`

--- apps/vehicles-registry-front-end/src/integrator/dev/dev.utility.ts
+++ apps/vehicles-registry-front-end/src/integrator/dev/dev.utility.ts
@@ -1,17 +1,17 @@
-export class DevUtility {
-  static readonly #vehiclesRegistryLogPrefix = '[Vehicles registry app dev]'
+export const devPrefix = '[Elaboration. Vehicles registry app dev]'
 
+export class DevUtility {
   static collapsedTable(
     ...groupOptionalParameters: readonly unknown[]
   ): (tabularData: unknown, columns?: readonly string[]) => void {
     return (tabularData: unknown, columns?: readonly string[]): void => {
-      console.groupCollapsed(this.#vehiclesRegistryLogPrefix, ...groupOptionalParameters)
+      console.groupCollapsed(devPrefix, ...groupOptionalParameters)
       console.table(tabularData, columns as string[])
       console.groupEnd()
     }
   }
 
   static info(...optionalParameters: readonly unknown[]): void {
-    console.info(this.#vehiclesRegistryLogPrefix, ...optionalParameters)
+    console.info(devPrefix, ...optionalParameters)
   }
 }
