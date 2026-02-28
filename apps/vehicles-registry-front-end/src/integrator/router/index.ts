5021393840a88100a88ee793bafdf84141a78707
derfex <derfex@yandex.ru>
2026-02-28 18:56:39 +0300

    #107. refactor(vehicles-registry-front-end → integrator): rename `PassengerVehiclesRegistryView`

--- apps/vehicles-registry-front-end/src/integrator/router/index.ts
+++ apps/vehicles-registry-front-end/src/integrator/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      component: (): RouteComponent => import('../ui/views/PassengerVehiclesView.vue'),
+      component: (): RouteComponent => import('../ui/views/PassengerVehiclesRegistryView.vue'),
       name: 'registry',
       path: '/',
     },
