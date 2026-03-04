d602b3838d11a85cddf576f51d47b9f10da2b275
derfex <derfex@yandex.ru>
2026-03-04 22:58:10 +0300

    #107. feat(vehicles-registry-front-end → integrator): resolve the `TODO`

--- apps/vehicles-registry-front-end/src/integrator/data-access/back-end-api/use-fetch.ts
+++ apps/vehicles-registry-front-end/src/integrator/data-access/back-end-api/use-fetch.ts
@@ -1,24 +1,27 @@
-import { type ShallowRef, shallowRef } from 'vue'
+import { computed, type ComputedRef, shallowRef } from 'vue'
 
 export function useFetch<TData, TError>(url: string, initialValue: TData): UseFetchResult<TData, TError> {
-  const data = shallowRef<TData>(initialValue)
-  const error = shallowRef<TError | null>(null)
-  const loading = shallowRef(true)
+  const _data = shallowRef<TData>(initialValue)
+  const _error = shallowRef<TError | null>(null)
+  const _loading = shallowRef(true)
 
   fetch(url)
     .then((res: Response): Promise<TData> => res.json())
-    .then((dataValue: TData): TData => (data.value = dataValue))
-    .catch((err: TError): TError => (error.value = err))
+    .then((dataValue: TData): TData => (_data.value = dataValue))
+    .catch((err: TError): TError => (_error.value = err))
     .finally((): void => {
-      loading.value = false
+      _loading.value = false
     })
 
-  // FIXME?: Check. Can we put a value in `data.value`?
+  const data = computed<TData>(() => _data.value)
+  const error = computed<TError | null>(() => _error.value)
+  const loading = computed(() => _loading.value)
+
   return { data, error, loading }
 }
 
 interface UseFetchResult<TData, TError> {
-  readonly data: ShallowRef<TData>
-  readonly error: ShallowRef<TError | null>
-  readonly loading: ShallowRef<boolean>
+  readonly data: ComputedRef<TData>
+  readonly error: ComputedRef<TError | null>
+  readonly loading: ComputedRef<boolean>
 }
