16b7f57a8c1ff074a70441612dbec1a9fab8cbbd
derfex <derfex@yandex.ru>
2026-03-07 18:15:50 +0300

    #107. feat(vehicles-registry-fe): create and use `passenger-vehicles-for-be-from-controlled-source.composable.ts`

--- apps/vehicles-registry-fe/src/integrator/data-access/back-end-api/passenger-vehicles/passenger-vehicles-mediator.composable.ts
+++ apps/vehicles-registry-fe/src/integrator/data-access/back-end-api/passenger-vehicles/passenger-vehicles-mediator.composable.ts
@@ -1,29 +1,40 @@
 import { computed, type ComputedRef } from 'vue'
 import type { PassengerVehicle } from '../../../../architecture/entities/passenger-vehicles/passenger-vehicles.type'
+import { usePassengerVehiclesForBEFromControlledSource } from './passenger-vehicles-for-be-from-controlled-source.composable'
 import { usePassengerVehiclesForBEFromPrioritySource } from './passenger-vehicles-for-be-from-priority-source.composable'
 import { usePassengerVehiclesFromLocalSource } from './passenger-vehicles-from-local-source.composable'
 import { convertToPassengerVehicles } from './passenger-vehicles.utility'
 
 export function usePassengerVehiclesMediator(): UsePassengerVehiclesMediatorResult {
+  // FIXME: How to implement a lazy way?
+  const _controlledSource = usePassengerVehiclesForBEFromControlledSource()
   const _prioritySource = usePassengerVehiclesForBEFromPrioritySource()
 
   // # API
 
   const error = computed<Error | null>(() => _prioritySource.error.value)
-  const list = computed<Vehicles>((): Vehicles => {
-    if (!_prioritySource.error.value) {
-      return convertToPassengerVehicles(_prioritySource.list.value)
-    }
-
-    const { list } = usePassengerVehiclesFromLocalSource()
-    return list.value
-  })
+  const list = computed<Vehicles>(() => _fetchList())
   const loading = computed<boolean>(() => _prioritySource.loading.value)
 
   return {
     error,
     list,
     loading,
+  }
+
+  // # Private logic
+
+  function _fetchList(): Vehicles {
+    if (!_prioritySource.error.value) {
+      return convertToPassengerVehicles(_prioritySource.list.value)
+    }
+
+    if (!_controlledSource.error.value) {
+      return convertToPassengerVehicles(_controlledSource.list.value)
+    }
+
+    const { list } = usePassengerVehiclesFromLocalSource()
+    return list.value
   }
 }
 
