953489d7ea9ea528c500d78b0563daa7d1cd703d
derfex <derfex@yandex.ru>
2026-03-13 23:10:15 +0300

    #91. refactor(philistine-calculations): specify the type

--- apps/philistine-calculations/vite.config.ts
+++ apps/philistine-calculations/vite.config.ts
@@ -3,52 +3,54 @@ import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig } from 'vite'
 
 // https://vitejs.dev/config
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/philistine-calculations',
-  server: {
-    host: 'localhost',
-    port: 4200,
-  },
-  preview: {
-    host: 'localhost',
-    port: 4300,
-  },
-  plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //   plugins: [nxViteTsPaths()],
-  // },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(
+  (): UserConfig => ({
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/philistine-calculations',
+    server: {
+      host: 'localhost',
+      port: 4200,
     },
-    emptyOutDir: true,
-    outDir: '../../dist/apps/philistine-calculations',
-    reportCompressedSize: true,
-  },
-  resolve: {
-    alias: {
-      '||': fileURLToPath(new URL('./src', import.meta.url)),
-      '|integrator': fileURLToPath(new URL('./src/integrator', import.meta.url)),
-      '|logic': fileURLToPath(new URL('./src/architecture/logic', import.meta.url)),
-      '|ui': fileURLToPath(new URL('./src/integrator/ui', import.meta.url)),
-      '|ui-kit': fileURLToPath(new URL('./src/integrator/ui-kit', import.meta.url)),
+    preview: {
+      host: 'localhost',
+      port: 4300,
     },
-  },
-  test: {
-    coverage: {
-      provider: 'v8' as const,
-      reportsDirectory: '../../coverage/apps/philistine-calculations',
+    plugins: [vue(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //   plugins: [nxViteTsPaths()],
+    // },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+      emptyOutDir: true,
+      outDir: '../../dist/apps/philistine-calculations',
+      reportCompressedSize: true,
     },
-    environment: 'jsdom',
-    globals: true,
-    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    name: 'philistine-calculations',
-    reporters: ['default'],
-    watch: false,
-  },
-}))
+    resolve: {
+      alias: {
+        '||': fileURLToPath(new URL('./src', import.meta.url)),
+        '|integrator': fileURLToPath(new URL('./src/integrator', import.meta.url)),
+        '|logic': fileURLToPath(new URL('./src/architecture/logic', import.meta.url)),
+        '|ui': fileURLToPath(new URL('./src/integrator/ui', import.meta.url)),
+        '|ui-kit': fileURLToPath(new URL('./src/integrator/ui-kit', import.meta.url)),
+      },
+    },
+    test: {
+      coverage: {
+        provider: 'v8' as const,
+        reportsDirectory: '../../coverage/apps/philistine-calculations',
+      },
+      environment: 'jsdom',
+      globals: true,
+      include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      name: 'philistine-calculations',
+      reporters: ['default'],
+      watch: false,
+    },
+  }),
+)
