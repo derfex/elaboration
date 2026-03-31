1e4a3c24cfa06610bd79a8ca3224dde77b8d17e3
derfex <derfex@yandex.ru>
2026-03-31 19:50:48 +0300

    #76. docs(derfex-profile): update the comments

--- apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
+++ apps/derfex-profile/src/app/integrator/ui/dx-activities/dx-activity-card/dx-activity-card.component.ts
@@ -10,11 +10,13 @@ export class DXActivityCardComponent {
   public readonly achievements = input.required<readonly string[]>()
   public readonly achievementsTitleText = input.required<string>()
   // TODO?: Display a caption, like a tag, like a category of an activity.
-  //  Examples: `web-studio`, `independent development.
+  //  Examples: 'web-studio', 'independent development'.
   public readonly period = input.required<string>()
-  // TODO?: Display length of the period. Example: `2021-01 – настоящее время · 4 г. 3 мес.`.
+  // TODO?: Display length of the period. Example: '2021-01 – настоящее время · 4 г. 3 мес.'.
   public readonly role = input.required<string>()
   public readonly shortDescription = input.required<string>()
   public readonly skills = input.required<readonly string[]>()
   public readonly skillsTitleText = input.required<string>()
+  // TODO?: Display a team description.
+  //  Examples: '10–15 people'.
 }
