40e59e68942f90f348df0b5918928bf085f270cb
derfex <derfex@yandex.ru>
2026-03-31 19:08:14 +0300

    #76. test(derfex-profile-e2e): specify the type

--- apps/derfex-profile-e2e/src/example.spec.ts
+++ apps/derfex-profile-e2e/src/example.spec.ts
@@ -1,8 +1,17 @@
-import { test, expect } from '@playwright/test'
+import {
+  expect,
+  type PlaywrightTestArgs,
+  type PlaywrightTestOptions,
+  type PlaywrightWorkerArgs,
+  type PlaywrightWorkerOptions,
+  test,
+} from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('has title', async ({
+  page,
+}: PlaywrightTestArgs & PlaywrightTestOptions & PlaywrightWorkerArgs & PlaywrightWorkerOptions): Promise<void> => {
   await page.goto('/')
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome')
+  // Expect `<h1>` to contain a substring.
+  expect(await page.locator('h1').innerText()).toContain('No data')
 })
