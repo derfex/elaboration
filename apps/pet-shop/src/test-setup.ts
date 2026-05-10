5ed370de50823f0859022cd45f39cd841848bd49
derfex <derfex@yandex.ru>
2026-05-10 13:47:08 +0300

    #81. build(root): delete the dependencies `@analogjs/vite-plugin-angular`, `@analogjs/vitest-angular` (amend)

--- apps/pet-shop/src/test-setup.ts
+++ apps/pet-shop/src/test-setup.ts
@@ -1,4 +1,3 @@
-import '@analogjs/vitest-angular/setup-zone'
 import { getTestBed } from '@angular/core/testing'
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing'
 
