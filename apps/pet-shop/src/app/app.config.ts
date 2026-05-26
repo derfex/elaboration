f549af70e00537e006f34f8ecb9dde92b8616fd3
derfex <derfex@yandex.ru>
2026-05-26 23:06:09 +0300

    refactor(pet-shop): optimize the imports. With #81

--- apps/pet-shop/src/app/app.config.ts
+++ apps/pet-shop/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withFetch } from '@angular/common/http'
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core'
+import { type ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { appRoutes } from './app.routes'
 
