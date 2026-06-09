c568b0d707ae2533dad06e0b0ac9d5544c8ea40c
derfex <derfex@yandex.ru>
2026-06-09 14:08:10 +0300

    #76. feat(derfex-profile-data-server): implement `debugMode`

--- apps/derfex-profile-data-server/src/app/save-data/save-data.service.ts
+++ apps/derfex-profile-data-server/src/app/save-data/save-data.service.ts
@@ -1,8 +1,10 @@
 import { Body, HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import * as fsExtra from 'fs-extra'
+import { access, constants } from 'node:fs/promises'
 import path from 'node:path'
 import type { SaveFilePostRequestBody, SaveFilePostResponse } from './save-data.type'
 
+const debugMode = false
 const filesMap: ReadonlyMap<string, string> = new Map()
 const pathPrefixes = [] as const
 
@@ -24,6 +26,15 @@ export class SaveDataService {
     const promises: Promise<void>[] = []
     pathPrefixes.forEach((prefix: string): void => {
       const filePath = path.join(prefix, fileName)
+      if (debugMode) {
+        this.#debugCheckAccess(prefix)
+          .then((): void => {
+            this.#debugHandleExistence(prefix, filePath)
+          })
+          .catch((): void => {
+            this.#debugHandleNonExistence(prefix, filePath)
+          })
+      }
       const promise = fsExtra.outputFile(filePath, content).catch((): never => {
         throw new HttpException('Error saving file.', HttpStatus.INTERNAL_SERVER_ERROR)
       })
@@ -36,5 +47,17 @@ export class SaveDataService {
     } catch {
       throw new HttpException('Error saving file.', HttpStatus.INTERNAL_SERVER_ERROR)
     }
+  }
+
+  #debugCheckAccess(path: string): Promise<void> {
+    return access(path, constants.F_OK)
+  }
+
+  #debugHandleExistence(path: string, filePath: string): void {
+    // The path exists.
+  }
+
+  #debugHandleNonExistence(path: string, filePath: string): void {
+    // The path does not exist.
   }
 }
